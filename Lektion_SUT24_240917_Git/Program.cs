a8e48eb87cd444a6c2caae98b171a510139906ff
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-09-17 10:11:21 +0200

    Add project files.

---
+++ Lektion_SUT24_240917_Git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lektion_SUT24_240917_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
